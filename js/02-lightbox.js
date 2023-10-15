3f3d7a67dcc543cd4d5b7f19900686141922c673
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-10-16 00:10:17 +0200

    00-10

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const createImagesListMarkup = (images) => images.map(({ original, preview, description }) => `
+<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+`).join("");
+
+const listImages = document.querySelector(".gallery");
+
+console.log(listImages);
+const imagesMarkup = galleryItems ? createImagesListMarkup(galleryItems) : '';
+listImages.insertAdjacentHTML('beforeend', imagesMarkup)
+
+const slider = new SimpleLightbox('.gallery a' , {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
